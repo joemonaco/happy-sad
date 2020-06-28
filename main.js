5bf35e0189867e585618960f3f0ee4bb81e3e301
Joe Monaco <jmonaco0506@gmail.com>
2020-06-28 19:28:21 -0400

    initial

---
+++ main.js
@@ -0,0 +1,19 @@
+
+days = 0;
+function addDay() {
+  document.g;
+  const good = document.getElementById("good-gif");
+  good.classList.remove("good-gif");
+  setTimeout(function() {
+    good.classList.add("good-gif");
+  }, 3000);
+}
+
+function reset() {
+
+  const bad = document.getElementById("bad-gif");
+  bad.classList.remove("bad-gif");
+  setTimeout(function() {
+    bad.classList.add("bad-gif");
+  }, 3000);
+}
